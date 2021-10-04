25d8fa25811ca1df947e0a947508026997a6c6ec
Nitin <info@contractorbhai.com>
2021-10-04 11:44:48 +0530

    second commit

--- resources/views/admin/customer/create.blade.php
+++ resources/views/admin/customer/create.blade.php
@@ -5,7 +5,7 @@
         <div class="card-header">
             <div class="card-header-container">
                 <h6 class="card-title">
-                    {{ trans('global.create') }}
+                    Nitin{{ trans('global.create') }}
                     {{ trans('cruds.customer.title_singular') }}
                 </h6>
             </div>
